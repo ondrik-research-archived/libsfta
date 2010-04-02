7492276f6a95e6d29b7e09086878ef8d5ecd31f2
Ondra Lengal <ondra@lengal.net>
2010-04-02 20:23:11 +0200

    Add stub for CUDDSharedMTBDD
     * Added stub for CUDDSharedMTBDD
    
     * Modified some properties of AbstractSharedMTBDD

--- include/knihovna/abstract_shared_mtbdd.hh
+++ include/knihovna/abstract_shared_mtbdd.hh
@@ -3,25 +3,32 @@
 
 
 
+#ifndef _ABSTRACT_SHARED_MTBDD_HH_
+#define _ABSTRACT_SHARED_MTBDD_HH_
+
+#include <string>
 
 
 namespace Knihovna
 {
+	template <typename RootType, class VariableAssignmentType, class LeafAllocator, class LeafAllocatorLeafType>
 	class AbstractSharedMTBDD;
 }
 
 
-
-
-
-template <typename RootType, class VariableAssignment, class LeafAllocator>
+template <typename RootType, class VariableAssignmentType, class LeafAllocator, class LeafAllocatorLeafType>
 class Knihovna::AbstractSharedMTBDD
 {
 public:
 
-	void SetValue(const RootType& root, const VariableAssignment& position, const LeafAllocator::LeafType& value) = 0;
-	LeafAllocator::LeafType& GetValue(const RootType& root, const VariableAssignment& position) = 0;
-	RootType Apply(const RootType& lhs, const RootType& rhs, ApplyFunction func) = 0;
-	RootType CreateRoot() = 0;
-	std::string Serialize() = 0;
+	typedef LeafAllocatorLeafType (*ApplyFunctionType)(LeafAllocatorLeafType, LeafAllocatorLeafType);
+
+	virtual void SetValue(const RootType& root, const VariableAssignmentType& position, const LeafAllocatorLeafType& value) = 0;
+	virtual LeafAllocatorLeafType& GetValue(const RootType& root, const VariableAssignmentType& position) = 0;
+	virtual RootType Apply(const RootType& lhs, const RootType& rhs, const ApplyFunctionType& func) = 0;
+	virtual RootType CreateRoot() = 0;
+	virtual std::string Serialize() const = 0;
+	virtual ~AbstractSharedMTBDD() { }
 };
+
+#endif
