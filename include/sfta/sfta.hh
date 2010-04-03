855be5fc5ca5a42036d2fcc62c6a18fb070d44fa
Ondra Lengal <ondra@lengal.net>
2010-04-03 20:00:51 +0200

    Add global header file sfta.hh
     - /include/sfta/sfta.hh header file with global declarations added.

---
+++ include/sfta/sfta.hh
@@ -0,0 +1,7 @@
+// Log4cpp headers
+#include <log4cpp/Category.hh>
+
+#define SFTA_LOGGER_PREFIX (std::string(__FILE__ ":" + SFTA::Private::Convert::ToString(__LINE__) + ": "))
+
+#define SFTA_LOGGER_WARN(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).warn((SFTA_LOGGER_PREFIX) + (msg)))
+
