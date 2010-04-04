e286559b86dc96026742060230525df79032d371
Ondra Lengal <ondra@lengal.net>
2010-04-04 13:11:26 +0200

    Add testing to CUDDFacade test :-)
     - CUDDFacade test now tests something

--- tests/cudd_facade_test.cc
+++ tests/cudd_facade_test.cc
@@ -1,4 +1,6 @@
 #include <sfta/cudd_facade.hh>
+using SFTA::Private::CUDDFacade;
+using SFTA::Private::CUDDFacade;
 
 #include <log4cpp/Category.hh>
 #include <log4cpp/OstreamAppender.hh>
@@ -9,10 +11,15 @@
 #include <boost/test/unit_test.hpp>
 
 
+/******************************************************************************
+ *                                  Fixtures                                  *
+ ******************************************************************************/
+
 class LogFixture
 {
 
 private:
+
 	static bool logInitialized_;
 
 public:
@@ -44,20 +51,79 @@ public:
 bool LogFixture::logInitialized_ = false;
 
 
-
-BOOST_FIXTURE_TEST_SUITE(suite, LogFixture)
-
-BOOST_AUTO_TEST_CASE(constructors)
+class CUDDFacadeFixture : public LogFixture
 {
-	SFTA::Private::CUDDFacade cudd;
-}
+protected:
 
-BOOST_AUTO_TEST_CASE(setters_and_getters_test)
-{
-}
+	CUDDFacade facade;
 
-BOOST_AUTO_TEST_CASE(serialization)
+public:
+
+	CUDDFacadeFixture()
+		: facade()
+	{ }
+};
+
+
+/******************************************************************************
+ *                              Start of testing                              *
+ ******************************************************************************/
+
+BOOST_FIXTURE_TEST_SUITE(suite, CUDDFacadeFixture)
+
+BOOST_AUTO_TEST_CASE(work_with_variables)
 {
+	CUDDFacade::Node* x0 = facade.AddIthVar(0);
+	BOOST_CHECK(x0 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(x0);
+
+	CUDDFacade::Node* not_x0 = facade.AddCmpl(x0);
+	BOOST_CHECK(not_x0 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(not_x0);
+
+	CUDDFacade::Node* x2 = facade.AddIthVar(2);
+	BOOST_CHECK(x2 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(x2);
+
+	CUDDFacade::Node* not_x2 = facade.AddCmpl(x2);
+	BOOST_CHECK(not_x2 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(not_x2);
+
+	CUDDFacade::Node* const_three = facade.AddConst(3);
+	BOOST_CHECK(const_three != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(const_three);
+
+	CUDDFacade::Node* const_seven = facade.AddConst(7);
+	BOOST_CHECK(const_seven != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(const_seven);
+
+	CUDDFacade::Node* const_hundred = facade.AddConst(100);
+	BOOST_CHECK(const_hundred != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(const_hundred);
+
+	CUDDFacade::Node* old_bck = facade.ReadBackground();
+	BOOST_CHECK(old_bck != static_cast<CUDDFacade::Node*>(0));
+	facade.SetBackground(const_hundred);
+	facade.RecursiveDeref(old_bck);
+
+	CUDDFacade::Node* three_x0 = facade.Times(const_three, x0);
+	BOOST_CHECK(three_x0 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(three_x0);
+
+	CUDDFacade::Node* three_x0_not_x2 = facade.Times(three_x0, not_x2);
+	BOOST_CHECK(three_x0_not_x2 != static_cast<CUDDFacade::Node*>(0));
+	facade.Ref(three_x0_not_x2);
+	facade.RecursiveDeref(three_x0);
+
+
+	facade.RecursiveDeref(three_x0_not_x2);
+	facade.RecursiveDeref(x0);
+	facade.RecursiveDeref(not_x0);
+	facade.RecursiveDeref(x2);
+	facade.RecursiveDeref(not_x2);
+	facade.RecursiveDeref(const_three);
+	facade.RecursiveDeref(const_seven);
+	facade.RecursiveDeref(const_hundred);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
