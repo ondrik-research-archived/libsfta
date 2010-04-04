a0ce7416f165ad58cf3d17709f8b330b73fc0027
Ondra Lengal <ondra@lengal.net>
2010-04-04 11:24:45 +0200

    CUDDFacade sanitized
     - CUDDFacade interface now offers nice Doxygen documentation.
    
     - Added implementation of CUDDFacade.

--- cudd_facade/cudd_facade.cc
+++ cudd_facade/cudd_facade.cc
@@ -1,3 +1,10 @@
+/*****************************************************************************
+ *  Symbolic Finite Tree Automata Library
+ *
+ *  Copyright (c) 2010  Ondra Lengal <ondra@lengal.net>
+ ****************************************************************************/
+
+
 // Standard library headers
 #include <stdexcept>
 
@@ -14,31 +21,168 @@
 using namespace SFTA::Private;
 
 
+// Setting the logging category name for Log4cpp
 const char* CUDDFacade::LOG_CATEGORY_NAME = "cudd_facade";
 
 
-CUDDFacade::CUDDFacade() : manager_(static_cast<DdManager*>(0))
+// A set of functions that converts between SFTA and CUDD manager and node
+// types. 
+namespace SFTA
 {
-	// Create the manager
-	if ((manager_ = Cudd_Init(0, 0, CUDD_UNIQUE_SLOTS, CUDD_CACHE_SLOTS, 0)) == static_cast<DdManager*>(0))
+	namespace Private
 	{
-		throw std::runtime_error("CUDD Manager could not be created");
+		/**
+		 * Converts a pointer to manager from DdManager type to
+		 * CUDDFacade::Manager type.
+		 *
+		 * @param[in]  cudd_value  CUDD DdManager pointer
+		 *
+		 * @returns  SFTA Manager pointer
+		 */
+		CUDDFacade::Manager* fromCUDD(DdManager* cudd_value)
+		{
+			return reinterpret_cast<CUDDFacade::Manager*>(cudd_value);
+		}
+
+
+		/**
+		 * Converts a pointer to manager from DdNode type to
+		 * CUDDFacade::Node type.
+		 *
+		 * @param[in]  cudd_value  CUDD DdNode pointer
+		 *
+		 * @returns  SFTA Node pointer
+		 */
+		CUDDFacade::Node* fromCUDD(DdNode* cudd_value)
+		{
+			return reinterpret_cast<CUDDFacade::Node*>(cudd_value);
+		}
+
+
+		/**
+		 * Converts a pointer to manager from CUDDFacade::Manager type to
+		 * DdManager type.
+		 *
+		 * @param[in]  sfta_value  SFTA CUDDFacade::Manager pointer
+		 *
+		 * @returns  CUDD DdManager pointer
+		 */
+		DdManager* toCUDD(CUDDFacade::Manager* sfta_value)
+		{
+			return reinterpret_cast<DdManager*>(sfta_value);
+		}
+
+
+		/**
+		 * Converts a pointer to manager from CUDDFacade::Node type to
+		 * DdNode type.
+		 *
+		 * @param[in]  sfta_value  SFTA CUDDFacade::Node pointer
+		 *
+		 * @returns  CUDD DdNode pointer
+		 */
+		DdNode* toCUDD(CUDDFacade::Node* sfta_value)
+		{
+			return reinterpret_cast<DdNode*>(sfta_value);
+		}
 	}
 }
+
+
+CUDDFacade::CUDDFacade()
+	: manager_(static_cast<Manager*>(0))
+{
+	// Create the manager
+	if ((manager_ = fromCUDD(Cudd_Init(0, 0, CUDD_UNIQUE_SLOTS, CUDD_CACHE_SLOTS, 0)))
+		== static_cast<Manager*>(0))
+	{	// in case the manager could not be created
+		std::string error_msg = "CUDD Manager could not be created";
+		SFTA_LOGGER_FATAL(error_msg);
+		throw std::runtime_error(error_msg);
+	}
+}
+
+
+CUDDFacade::Node* CUDDFacade::AddIthVar(int i)
+{
+	// Assertions
+	assert(i >= 0);
+
+	return fromCUDD(Cudd_addIthVar(toCUDD(manager_), i));
+}
+
+
+SFTA::Private::CUDDFacade::Node* CUDDFacade::AddCmpl(Node* node)
+{
+	// Assertions
+	assert(node != static_cast<Node*>(0));
+
+	return fromCUDD(Cudd_addCmpl(toCUDD(manager_), toCUDD(node)));
+}
+
+
+CUDDFacade::Node* CUDDFacade::AddConst(CUDDFacade::ValueType value)
+{
+	return fromCUDD(Cudd_addConst(toCUDD(manager_), value));
+}
+
+
+void CUDDFacade::Ref(Node* node)
+{
+	// Assertions
+	assert(node != static_cast<Node*>(0));
+
+	Cudd_Ref(toCUDD(node));
+}
+
+
+void CUDDFacade::RecursiveDeref(Node* node)
+{
+	// Assertions
+	assert(node != static_cast<Node*>(0));
+
+	Cudd_RecursiveDeref(toCUDD(manager_), toCUDD(node));
+}
+
+
+void CUDDFacade::SetBackground(Node* bck)
+{
+	// Assertions
+	assert(bck != static_cast<Node*>(0));
+
+	Cudd_SetBackground(toCUDD(manager_), toCUDD(bck));
+}
+
+
+CUDDFacade::Node* CUDDFacade::ReadBackground()
+{
+	return fromCUDD(Cudd_ReadBackground(toCUDD(manager_)));
+}
+
+
+CUDDFacade::Node* CUDDFacade::Times(Node* lhs, Node* rhs)
+{
+	// Assertions
+	assert(!(cuddIsConstant(toCUDD(lhs)) && cuddIsConstant(toCUDD(rhs))));
+
+	return fromCUDD(Cudd_addApply(toCUDD(manager_), Cudd_addTimes,
+		toCUDD(lhs), toCUDD(rhs)));
+}
+
 
 CUDDFacade::~CUDDFacade()
 {
 	// Assertions
-	assert(manager_ != static_cast<DdManager*>(0));
+	assert(manager_ != static_cast<Manager*>(0));
 
 	// Check for nodes with non-zero reference count
 	int unrefed = 0;
-	if ((unrefed = Cudd_CheckZeroRef(manager_)) != 0)
+	if ((unrefed = Cudd_CheckZeroRef(toCUDD(manager_))) != 0)
 	{	// in case there are still some nodes unreferenced
 		SFTA_LOGGER_WARN("Still " + Convert::ToString(unrefed) + " nodes unreferenced!");
 	}
 
 	// Delete the manager
-	Cudd_Quit(manager_);
-	manager_ = static_cast<DdManager*>(0);
+	Cudd_Quit(toCUDD(manager_));
+	manager_ = static_cast<Manager*>(0);
 }
--- cudd_facade/cudd_facade.hh
+++ cudd_facade/cudd_facade.hh
@@ -1,48 +1,229 @@
+/*****************************************************************************
+ *  Symbolic Finite Tree Automata Library
+ *
+ *  Copyright (c) 2010  Ondra Lengal <ondra@lengal.net>
+ ****************************************************************************/
 
-namespace SFTA
-{
-	namespace Private
-	{
-		class CUDDFacade;
-	}
-}
 
-// Declaration of structures from CUDD... We don't want to include CUDD header
-// files into a header file, because they are horrible.
-struct DdManager;
-struct DdNode;
+// insert the class into proper namespace
+namespace SFTA { namespace Private { class CUDDFacade; } }
 
+
+/**
+ * @brief   Facade for CUDD package
+ * @author  Ondra Lengal <ondra@lengal.net>
+ * @date    2010
+ *
+ * A class that serves as a facade to the CUDD package. It provides means to
+ * work with CUDD with no CUDD declarations, which means that no CUDD header
+ * need to be included in files using CUDDFacade. This facade provides access
+ * to a single CUDD manager and handles its construction and destruction. It
+ * also takes care not to let any CUDD-specific type spoil global namespace.
+ */
 class SFTA::Private::CUDDFacade
 {
-private:	// Private data members
+public:  // Public types
 
-	DdManager* manager_;
-
-
-public:	// Public data members
-
-	static const char* LOG_CATEGORY_NAME;
-
+	/**
+	 * @brief  MTBDD sink node type
+	 *
+	 * The type in sink nodes of the MTBDD. Declared here to be easily modifiable.
+	 * Note that it needs to correspond to CUDD_VALUE_TYPE in CUDD.
+	 */
 	typedef unsigned ValueType;
 
-private:
 
+	/**
+	 * @brief  Type of MTBDD manager
+	 *
+	 * The type used for manipulation with MTBDD manager. Note that this type is
+	 * a correct-namespace substitute for CUDD's DdManager and is always
+	 * transformed to this inside the facade. This type is never defined!
+	 */
+	struct Manager;
+
+
+	/**
+	 * @brief  Type of MTBDD node
+	 *
+	 * The type used for manipulation with MTBDD node. Note that this type is
+	 * a correct-namespace substitute for CUDD's DdNode and is always
+	 * transformed to this inside the facade. This type is never defined!
+	 */
+	struct Node;
+
+
+public:  // Public data members
+
+	/**
+	 * @brief  Logging category name for Log4cpp
+	 *
+	 * Name of the logging category that this class uses for logging using
+	 * Log4cpp.
+	 */
+	static const char* LOG_CATEGORY_NAME;
+
+
+private: // Private data members
+
+	/**
+	 * @brief  MTBDD manager
+	 *
+	 * Manager that takes care of used MTBDD.
+	 */
+	Manager* manager_;
+
+
+private: // Private methods
+
+	/**
+	 * Private copy constructor that disables copying.
+	 *
+	 * @param[in]  facade  CUDD facade to be copied
+	 */
 	CUDDFacade(const CUDDFacade& facade);
-	CUDDFacade& operator=(const CUDDFacade& lhs);
 
-public:
+
+	/**
+	 * Private assignment operator that disables assignment.
+	 *
+	 * @param[in]  rhs  Right-hand side of the assignment
+	 *
+	 * @returns  Copied instance
+	 */
+	CUDDFacade& operator=(const CUDDFacade& rhs);
+
+
+public:  // Public methods
+
+	/**
+	 * @brief  Implicit constructor
+	 *
+	 * Implicit constructor of CUDDFacade. Initializes the CUDD manager.
+	 */
 	CUDDFacade();
 
-	DdNode* AddIthVar(int i);
-	DdNode* AddCmpl(DdNode* node);
-	DdNode* AddConst(ValueType value);
-	void Ref(DdNode* node);
-	DdNode* ReadBackground();
-	void SetBackground(DdNode* bck);
-	void RecursiveDeref(DdNode* node);
-	DdNode* Times(DdNode* lsh, DdNode* rhs);
+
+	/**
+	 * @brief  Adds a variable to MTBDD
+	 *
+	 * Adds a variable on the i-th index to the MTBDD.
+	 *
+	 * @see  AddConst(ValueType value)
+	 *
+	 * @param[in]  i  Index of the variable
+	 *
+	 * @returns  Node representing the variable
+	 */
+	Node* AddIthVar(int i);
 
 
+	/**
+	 * @brief  Adds the complement of a variable
+	 *
+	 * For variable \f$ x \f$ generates the complement of \f$ x \f$: \f$ \neg x \f$.
+	 *
+	 * @see  AddIthVar(int i)
+	 *
+	 * @param[in]  node  Node of the variable to be complemented
+	 *
+	 * @returns  Complement of \p node
+	 */
+	Node* AddCmpl(Node* node);
+
+	/**
+	 * @brief  Adds a new constant
+	 *
+	 * This function creates a new sink node of the MTBDD of type ValueType and
+	 * returns pointer to it.
+	 *
+	 * @see  addIthVar(int i)
+	 *
+	 * @param[in]  value  Value of the constant
+	 *
+	 * @returns  Pointer to the node that contains the value
+	 */
+	Node* AddConst(ValueType value);
+
+
+	/**
+	 * @brief  References a node
+	 *
+	 * Increments the reference counter of given node by one. The reference
+	 * counter is used for garbage collection of nodes when the nodes are
+	 * dereferenced by RecursiveDeref(Node* node).
+	 *
+	 * @see  RecursiveDeref(Node* node)
+	 *
+	 * @param[in]  node  The node to be referenced
+	 */
+	void Ref(Node* node);
+
+
+	/**
+	 * @brief  Recursively dereferences a node
+	 *
+	 * Decrements the reference counter of given node previously referenced by
+	 * Ref(Node* node) and recursively executes the same step on all other nodes
+	 * pointed by the node.
+	 *
+	 * @see  Ref(Node* node)
+	 *
+	 * @param[in]  node  The node to be recursively dereferenced
+	 */
+	void RecursiveDeref(Node* node);
+
+
+	/**
+	 * @brief  Sets the background value of MTBDD
+	 *
+	 * Given a node, sets the background value of the MTBDD (i.e. the default
+	 * value for all assignments that are not explicitly given) to this node.
+	 * The value can be later read by ReadBackground().
+	 *
+	 * @see  ReadBackground()
+	 *
+	 * @param[in]  bck  The background value
+	 */
+	void SetBackground(Node* bck);
+
+
+	/**
+	 * @brief  Reads the background value of MTBDD
+	 *
+	 * Reads the node with the background value of MTBDD set previously by
+	 * SetBackground(Node* bck).
+	 *
+	 * @see  SetBackground(Node* bck)
+	 *
+	 * @returns  The background value of MTBDD
+	 */
+	Node* ReadBackground();
+
+
+	/**
+	 * @brief  Multiplication of two nodes
+	 *
+	 * Performs either:
+	 *   @li  logical conjunction of two Boolean expressions, or
+	 *   @li  multiplication of a value of ValueType by a Boolean expression
+	 *
+	 * depending on the types of nodes passed to the method. Note that two nodes
+	 * of ValueType type should never be passed.
+	 *
+	 * @param[in]  lhs  Left-hand side of multiplication
+	 * @param[in]  rhs  Right-hand side of multiplication
+	 *
+	 * @returns  The node with the product
+	 */
+	Node* Times(Node* lhs, Node* rhs);
+
+
+	/**
+	 * @brief  The destructor
+	 *
+	 * Destructor of CUDDFacade. Releases any resources used by the CUDD
+	 * manager.
+	 */
 	~CUDDFacade();
-
 };
