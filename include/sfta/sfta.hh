82005398228b48ecb174ccaf789f8da93984bd0a
Ondra Lengal <ondra@lengal.net>
2010-04-05 21:52:15 +0200

    Add logger debug message macro
     - Macro to write debug messages to logger added.

--- include/sfta/sfta.hh
+++ include/sfta/sfta.hh
@@ -14,6 +14,7 @@
 
 #define SFTA_LOGGER_PREFIX (std::string(__FILE__ ":" + SFTA::Private::Convert::ToString(__LINE__) + ": "))
 
+#define SFTA_LOGGER_DEBUG(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).debug((SFTA_LOGGER_PREFIX) + (msg)))
 #define SFTA_LOGGER_WARN(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).warn((SFTA_LOGGER_PREFIX) + (msg)))
 #define SFTA_LOGGER_FATAL(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).fatal((SFTA_LOGGER_PREFIX) + (msg)))
 
