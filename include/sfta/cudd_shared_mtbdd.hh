2d30d3064b5684a91ff51719a739c69d0daa74d9
Ondra Lengal <ondra@lengal.net>
2010-04-05 21:53:28 +0200

    Add support to get value for given variable assignment to CUDDSharedMTBDD
     - CUDDSharedMTBDD now supports getting a value from a leaf determined by
       given variable assignment.

--- include/sfta/cudd_shared_mtbdd.hh
+++ include/sfta/cudd_shared_mtbdd.hh
@@ -13,10 +13,13 @@
 
 // Standard library headers
 #include <cassert>
+#include <vector>
 
 // SFTA headers
+#include <sfta/sfta.hh>
 #include <sfta/abstract_shared_mtbdd.hh>
 #include <sfta/cudd_facade.hh>
+#include <sfta/convert.hh>
 
 
 namespace SFTA
@@ -65,7 +68,8 @@ template
 	typename Leaf,
 	class VariableAssignmentType,
 	template <typename, typename> class LeafAllocator,
-	template <typename, typename> class RootAllocator>
+	template <typename, typename> class RootAllocator
+>
 class SFTA::CUDDSharedMTBDD
 	: public AbstractSharedMTBDD
 		<
@@ -146,6 +150,30 @@ private:   // Private data types
 		{
 			return rhs;
 		}
+
+		static typename LA::HandleType projectByRight(typename LA::HandleType lhs, typename LA::HandleType rhs, void* /*data*/)
+		{
+			if (rhs == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return lhs;
+			}
+		}
+
+		static typename LA::HandleType collectLeaves(typename LA::HandleType node, void* data)
+		{
+			std::vector<unsigned>& vec = *(static_cast<std::vector<unsigned>*>(data));
+
+			if (node != 0)
+			{
+				vec.push_back(node);
+			}
+
+			return node;
+		}
 	};
 
 
@@ -161,6 +189,7 @@ private:  // Private data members
 	 */
 	SFTA::Private::CUDDFacade cudd;
 
+	static const char* LOG_CATEGORY_NAME;
 
 private:  // Private methods
 
@@ -168,6 +197,24 @@ private:  // Private methods
 	{
 		CUDDFacade::ValueType leaf = LA::createLeaf(value);
 		CUDDFacade::Node* node = cudd.AddConst(leaf);
+		cudd.Ref(node);
+		for (unsigned i = 0; i < vars.Size(); ++i)
+		{
+			CUDDFacade::Node* var = cudd.AddIthVar(i);
+			cudd.Ref(var);
+			CUDDFacade::Node* tmp = cudd.Times(node, var);
+			cudd.Ref(tmp);
+			cudd.RecursiveDeref(node);
+			cudd.RecursiveDeref(var);
+			node = tmp;
+		}
+
+		return RA::allocateRoot(node);
+	}
+
+	RootType createMTBDDForVariableProjection(const VariableAssignmentType& vars)
+	{
+		CUDDFacade::Node* node = cudd.AddConst(1);
 		cudd.Ref(node);
 		for (unsigned i = 0; i < vars.Size(); ++i)
 		{
@@ -191,7 +238,10 @@ public:   // Public methods
 	 * The constructor of CUDDSharedMTBDD.
 	 */
 	CUDDSharedMTBDD() : cudd()
-	{ }
+	{
+		// TODO: fix this
+		LA::createLeaf(0);
+	}
 
 
 	virtual void SetValue(const RootType& root, const VariableAssignmentType& asgn, const LeafType& value)
@@ -208,10 +258,30 @@ public:   // Public methods
 	}
 
 
-	virtual LeafType& GetValue(const RootType& /*root*/, const VariableAssignmentType& /*position*/)
+	virtual LeafType& GetValue(const RootType& root, const VariableAssignmentType& asgn)
 	{
-		static int smeti;
-		return reinterpret_cast<LeafType&>(smeti);
+		RootType mtbddAsgn = createMTBDDForVariableProjection(asgn);
+		CUDDFacade::ApplyCallbackParameters params(ApplyFunctions::projectByRight, static_cast<void*>(this));
+		CUDDFacade::Node* res = cudd.Apply(RA::getHandleOfRoot(root), RA::getHandleOfRoot(mtbddAsgn), &params);
+
+		std::vector<unsigned> vec;
+		CUDDFacade::MonadicApplyCallbackParameters paramsMon(ApplyFunctions::collectLeaves, static_cast<void*>(&vec));
+		cudd.MonadicApply(res, &paramsMon);
+
+
+		std::string pokus = "";
+		for (unsigned i = 0; i < vec.size(); ++i)
+		{
+			pokus += SFTA::Private::Convert::ToString(LA::getLeafOfHandle(vec[i])) + "; ";
+		}
+
+		SFTA_LOGGER_DEBUG("Elements in vector: "
+			+ SFTA::Private::Convert::ToString(vec.size()));
+		SFTA_LOGGER_DEBUG("Values of elements in vector: " + pokus);
+
+		assert(vec.size() > 0);
+
+		return LA::getLeafOfHandle(vec[0]);
 	}
 
 
@@ -247,5 +317,15 @@ public:   // Public methods
 		}
 	}
 };
+
+template
+<
+	typename Root,
+	typename Leaf,
+	class VariableAssignmentType,
+	template <typename, typename> class LeafAllocator,
+	template <typename, typename> class RootAllocator
+>
+const char* SFTA::CUDDSharedMTBDD<Root, Leaf, VariableAssignmentType, LeafAllocator, RootAllocator>::LOG_CATEGORY_NAME = "cudd_shared_mtbdd";
 
 #endif
