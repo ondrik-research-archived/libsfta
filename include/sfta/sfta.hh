2beb82a69193800744ebea1dd0d707ee248db7e8
Ondra Lengal <ondra@lengal.net>
2010-04-09 17:07:20 +0200

    Add all possible logger priorities to macros
     - All possible logger priorities from Log4cpp were added as macros to sfta.hh

--- include/sfta/sfta.hh
+++ include/sfta/sfta.hh
@@ -15,6 +15,11 @@
 #define SFTA_LOGGER_PREFIX (std::string(__FILE__ ":" + SFTA::Private::Convert::ToString(__LINE__) + ": "))
 
 #define SFTA_LOGGER_DEBUG(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).debug((SFTA_LOGGER_PREFIX) + (msg)))
+#define SFTA_LOGGER_INFO(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).info((SFTA_LOGGER_PREFIX) + (msg)))
+#define SFTA_LOGGER_NOTICE(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).notice((SFTA_LOGGER_PREFIX) + (msg)))
 #define SFTA_LOGGER_WARN(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).warn((SFTA_LOGGER_PREFIX) + (msg)))
+#define SFTA_LOGGER_ERROR(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).error((SFTA_LOGGER_PREFIX) + (msg)))
+#define SFTA_LOGGER_CRIT(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).crit((SFTA_LOGGER_PREFIX) + (msg)))
+#define SFTA_LOGGER_ALERT(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).alert((SFTA_LOGGER_PREFIX) + (msg)))
 #define SFTA_LOGGER_FATAL(msg) (log4cpp::Category::getInstance(LOG_CATEGORY_NAME).fatal((SFTA_LOGGER_PREFIX) + (msg)))
 
