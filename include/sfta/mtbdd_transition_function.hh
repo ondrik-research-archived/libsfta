c9878b4de7e67f8fdd27bc2932ffe39e41409913
Ondra Lengal <ondra@lengal.net>
2010-04-18 22:30:31 +0200

    MTBDDTransitionFunction: change
     - Changes in MTBDDTransitionFunction.

--- include/sfta/mtbdd_transition_function.hh
+++ include/sfta/mtbdd_transition_function.hh
@@ -8,8 +8,8 @@
  *
  *****************************************************************************/
 
-#ifndef _MTBDD_TRANSITION_FUNCTION_HH_
-#define _MTBDD_TRANSITION_FUNCTION_HH_
+#ifndef _SFTA_MTBDD_TRANSITION_FUNCTION_HH_
+#define _SFTA_MTBDD_TRANSITION_FUNCTION_HH_
 
 // SFTA headers
 #include <sfta/sfta.hh>
@@ -19,10 +19,15 @@
 // Standard library headers
 #include <cassert>
 #include <vector>
+#include <list>
 #include <tr1/unordered_map>
 
 // Boost library headers
 #include <boost/functional/hash.hpp>
+
+// Loki library header files
+#include <loki/HierarchyGenerators.h>
+#include <loki/TypelistMacros.h>
 
 
 // insert the class into proper namespace
@@ -104,13 +109,22 @@ class SFTA::MTBDDTransitionFunction
 {
 public:   // Public data types
 
-	typedef Symbol SymbolType;
-	typedef State StateType;
-	typedef LeftHandSide<StateType> LeftHandSideType;
-	typedef InputRightHandSide<StateType> InputRightHandSideType;
-	typedef OutputRightHandSide<StateType> OutputRightHandSideType;
+	typedef AbstractTransitionFunction<Symbol, State, LeftHandSide, InputRightHandSide, OutputRightHandSide> ParentClass;
+
+	typedef typename ParentClass::SymbolType SymbolType;
+	typedef typename ParentClass::StateType StateType;
+	typedef typename ParentClass::LeftHandSideType LeftHandSideType;
+	typedef typename ParentClass::InputRightHandSideType InputRightHandSideType;
+	typedef typename ParentClass::OutputRightHandSideType OutputRightHandSideType;
 	typedef MTBDD MTBDDType;
 	typedef MTBDDRoot MTBDDRootType;
+
+	typedef typename ParentClass::TransitionType TransitionType;
+	typedef typename ParentClass::TransitionListType TransitionListType;
+
+	typedef Loki::Tuple<LOKI_TYPELIST_2(SymbolType, InputRightHandSideType)>
+		RHSSymbolType;
+	typedef std::list<RHSSymbolType> RHSSymbolListType;
 
 	typedef std::vector<MTBDDRootType> MTBDDRootTypeArray;
 	typedef std::vector<MTBDDRootTypeArray> MTBDDRootTypeMatrix;
@@ -126,6 +140,10 @@ private:  // Private data types
 	 */
 	typedef SFTA::Private::Convert Convert;
 
+	typedef Loki::Tuple<LOKI_TYPELIST_2(LeftHandSideType, MTBDDRootType)>
+		LHSRootPair;
+
+	typedef std::list<LHSRootPair> LHSRootList;
 
 private:  // Private data members
 
@@ -139,7 +157,6 @@ private:  // Private data members
 	StateType sinkState_;
 
 
-	MTBDDRootType container0_;
 	MTBDDRootTypeArray container1_;
 	MTBDDRootTypeMatrix container2_;
 	MTBDDRootTypeHashTable containerN_;
@@ -158,21 +175,15 @@ private:  // Private methods
 
 	MTBDDRootType getRoot(const LeftHandSideType& lhs) const
 	{
+		// Assertions
+		assert(lhs.size() > 0);
+
 		switch (lhs.size())
 		{
-			case 0: return getRootForArity0(lhs); break;
 			case 1: return getRootForArity1(lhs); break;
 			case 2: return getRootForArity2(lhs); break;
 			default: return getRootForArityN(lhs); break;
 		}
-	}
-
-	MTBDDRootType getRootForArity0(const LeftHandSideType& lhs) const
-	{
-		// Assertions
-		assert(lhs.size() == 0);
-
-		return container0_;
 	}
 
 	MTBDDRootType getRootForArity1(const LeftHandSideType& lhs) const
@@ -208,21 +219,15 @@ private:  // Private methods
 
 	void setRoot(const LeftHandSideType& lhs, MTBDDRootType root)
 	{
+		// Assertions
+		assert(lhs.size() > 0);
+
 		switch (lhs.size())
 		{
-			case 0: return setRootForArity0(lhs, root); break;
 			case 1: return setRootForArity1(lhs, root); break;
 			case 2: return setRootForArity2(lhs, root); break;
 			default: return setRootForArityN(lhs, root); break;
 		}
-	}
-
-	void setRootForArity0(const LeftHandSideType& lhs, MTBDDRootType root)
-	{
-		// Assertions
-		assert(lhs.size() == 0);
-
-		container0_ = root;
 	}
 
 	void setRootForArity1(const LeftHandSideType& lhs, MTBDDRootType root)
@@ -249,20 +254,156 @@ private:  // Private methods
 		containerN_[lhs] = root;
 	}
 
+	LHSRootList getAllRootsForArity1() const
+	{
+		LHSRootList lst;
+
+		for (size_t i = 0; i < container1_.size(); ++i)
+		{	// append all non-bottom roots
+			if (container1_[i] != sinkState_)
+			{
+				LeftHandSideType lhs(1);
+				lhs[0] = i;
+
+				LHSRootPair pair;
+				Loki::Field<0>(pair) = lhs;
+				Loki::Field<1>(pair) = container1_[i];
+				lst.push_back(pair);
+			}
+		}
+
+		return lst;
+	}
+
+	LHSRootList getAllRootsForArity2() const
+	{
+		LHSRootList lst;
+
+		for (size_t i = 0; i < container2_.size(); ++i)
+		{
+			const MTBDDRootTypeArray& innerArr = container2_[i];
+
+			for (size_t j = 0; j < innerArr.size(); ++j)
+			{	// append all non-bottom roots
+				if (innerArr[j] != sinkState_)
+				{
+					LeftHandSideType lhs(2);
+					lhs[0] = i;
+					lhs[1] = j;
+
+					LHSRootPair pair;
+					Loki::Field<0>(pair) = lhs;
+					Loki::Field<1>(pair) = innerArr[j];
+					lst.push_back(pair);
+				}
+			}
+		}
+
+		return lst;
+	}
+
+	LHSRootList getAllRootsForArityN() const
+	{
+		LHSRootList lst;
+
+		for (typename MTBDDRootTypeHashTable::const_iterator it
+			= containerN_.begin(); it != containerN_.end(); ++it)
+		{	// append all non-bottom roots
+			if (it->second != sinkState_)
+			{
+				LHSRootPair pair;
+				Loki::Field<0>(pair) = it->first;
+				Loki::Field<1>(pair) = it->second;
+				lst.push_back(pair);
+			}
+		}
+
+		return lst;
+	}
+
+
+	RHSSymbolListType getTransitionsForSymbol(MTBDDRootType root,
+		SymbolType symbol, size_t position)
+	{
+		// Assertions
+		assert(position <= SymbolType::VariablesCount);
+
+		SFTA_LOGGER_DEBUG("Getting transition from root "
+			+ Convert::ToString(root) + " for symbol "
+			+ Convert::ToString(symbol) + " and position "
+			+ Convert::ToString(position));
+
+		if (position == SymbolType::VariablesCount)
+		{	// if we are at the bottom
+			RHSSymbolType rhsSymbol;
+			Loki::Field<0>(rhsSymbol) = symbol;
+
+			Loki::Field<1>(rhsSymbol) = *(mtbdd_.GetValue(root, symbol)[0]);
+
+			typename MTBDDType::LeafContainer leaves = mtbdd_.GetValue(root, symbol);
+			for (typename MTBDDType::LeafContainer::const_iterator it
+				= leaves.begin(); it != leaves.end(); ++it)
+			{
+				if ((**it)[0] != sinkState_)
+				{
+					Loki::Field<1>(rhsSymbol) = **it;
+				}
+			}
+
+			RHSSymbolListType result;
+
+			if (Loki::Field<1>(rhsSymbol)[0] != sinkState_)
+			{
+				result.push_back(rhsSymbol);
+			}
+
+			return result;
+		}
+		else
+		{	// in case we are not at the bottom
+			SymbolType restrictionToZero = symbol;
+			restrictionToZero.SetIthVariableValue(position, SymbolType::ZERO);
+			SymbolType restrictionToOne = symbol;
+			restrictionToOne.SetIthVariableValue(position, SymbolType::ONE);
+
+			typename MTBDDType::LeafContainer zeroLeaves
+				= mtbdd_.GetValue(root, restrictionToZero);
+			typename MTBDDType::LeafContainer oneLeaves
+				= mtbdd_.GetValue(root, restrictionToOne);
+
+			if (zeroLeaves == oneLeaves)
+			{	// in case variable at position is not important
+				return getTransitionsForSymbol(root, symbol, position + 1);
+			}
+			else
+			{	// in case leaves differ depending on the variable
+				RHSSymbolListType resultZero = getTransitionsForSymbol(root,
+					restrictionToZero, position + 1);
+				RHSSymbolListType resultOne = getTransitionsForSymbol(root,
+					restrictionToOne, position + 1);
+
+				resultZero.insert(resultZero.end(), resultOne.begin(), resultOne.end());
+				return resultZero;
+			}
+		}
+	}
 
 public:   // Public methods
 
 	MTBDDTransitionFunction(size_t statesCount, const StateType& sinkState)
 		: mtbdd_(),
-			sinkState_(sinkState),
-		  container0_(sinkState),
-			container1_(statesCount, sinkState),
-			container2_(statesCount, MTBDDRootTypeArray(statesCount, sinkState)),
+			sinkState_(),
+			container1_(),
+			container2_(),
 			containerN_()
 	{
 		InputRightHandSideType rhs;
 		rhs.push_back(sinkState);
 		mtbdd_.SetBottomValue(rhs);
+
+		sinkState_ = mtbdd_.GetRootForBottom();
+		container1_ = MTBDDRootTypeArray(statesCount, mtbdd_.GetRootForBottom());
+		container2_ = MTBDDRootTypeMatrix(statesCount, MTBDDRootTypeArray(statesCount, mtbdd_.GetRootForBottom()));
 	}
 
 
@@ -307,6 +448,55 @@ public:   // Public methods
 		}
 
 		return res;
+	}
+
+	inline MTBDDType& GetMTBDD()
+	{
+		return mtbdd_;
+	}
+
+	virtual TransitionListType GetListOfTransitions()
+	{
+		LHSRootList lst;
+		LHSRootList arity1Lst = getAllRootsForArity1();
+		lst.insert(lst.end(), arity1Lst.begin(), arity1Lst.end());
+		LHSRootList arity2Lst = getAllRootsForArity2();
+		lst.insert(lst.end(), arity2Lst.begin(), arity2Lst.end());
+		LHSRootList arityNLst = getAllRootsForArityN();
+		lst.insert(lst.end(), arityNLst.begin(), arityNLst.end());
+
+		TransitionListType result;
+
+		for (typename LHSRootList::const_iterator it = lst.begin();
+			it != lst.end(); ++it)
+		{	// for every valid root
+			const LHSRootPair& pair = *it;
+
+			RHSSymbolListType rhsSymbolLst;
+			typename SymbolType::AssignmentList asgns =
+				SymbolType::GetAllAssignments();
+
+			for (typename SymbolType::AssignmentList::const_iterator jt
+				= asgns.begin(); jt != asgns.end(); ++jt)
+			{	// for every assignment of variables to the symbol
+				const Symbol& symbol = *jt;
+				RHSSymbolListType lstSinks
+					= getTransitionsForSymbol(Loki::Field<1>(pair), symbol, 0);
+
+				for (typename RHSSymbolListType::const_iterator kt = lstSinks.begin();
+					kt != lstSinks.end(); ++kt)
+				{	// for all sinks in the list
+					TransitionType trans;
+					Loki::Field<0>(trans) = Loki::Field<0>(*kt);
+					Loki::Field<1>(trans) = Loki::Field<0>(pair);
+					Loki::Field<2>(trans) = Loki::Field<1>(*kt);
+
+					result.push_back(trans);
+				}
+			}
+		}
+
+		return result;
 	}
 
 
