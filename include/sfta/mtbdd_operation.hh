970694c022edc14248247212266555fc9db24e80
Ondra Lengal <ondra@lengal.net>
2010-04-21 08:00:39 +0200

    AbstractOperation: add basic implementation
     - Basic implementation of AbstractOperation and MTBDDOperation added.

---
+++ include/sfta/mtbdd_operation.hh
@@ -0,0 +1,93 @@
+/*****************************************************************************
+ *  Symbolic Finite Tree Automata Library
+ *
+ *  Copyright (c) 2010  Ondra Lengal <ondra@lengal.net>
+ *
+ *  Description:
+ *    The file with the MTBDDOperation class.
+ *
+ *****************************************************************************/
+
+#ifndef _SFTA_MTBDD_OPERATION_HH_
+#define _SFTA_MTBDD_OPERATION_HH_
+
+// SFTA header files
+#include <sfta/abstract_operation.hh>
+
+// Standard library header files
+#include <algorithm>
+
+// insert the class into proper namespace
+namespace SFTA
+{
+	template
+	<
+		class TreeAutomaton
+	>
+	class MTBDDOperation;
+}
+
+
+template
+<
+	class TreeAutomaton
+>
+class SFTA::MTBDDOperation
+	: public SFTA::AbstractOperation<TreeAutomaton>
+{
+private:  // Private data types
+
+	typedef typename TreeAutomaton::TransFuncPtrType TransFuncPtrType;
+
+	typedef typename TreeAutomaton::TransitionFunctionType TransitionFunctionType;
+
+	typedef typename TransitionFunctionType::StateType StateType;
+
+	typedef typename TransitionFunctionType::LeftHandSideType LeftHandSideType;
+
+	typedef typename TransitionFunctionType::MTBDDType MTBDDType;
+
+	typedef typename MTBDDType::RootType RootType;
+
+	typedef typename MTBDDType::LeafType LeafType;
+
+
+private:  // Private methods
+
+	static LeafType leafUnion(const LeafType& lhs, const LeafType& rhs)
+	{
+		return lhs.Union(rhs);
+	}
+
+
+public:   // Public methods
+
+	virtual TreeAutomaton Union(TreeAutomaton& ta1, TreeAutomaton& ta2) const
+	{
+		// Assertions
+		assert(ta1.GetTransitionFunction() == ta2.GetTransitionFunction());
+
+		TransFuncPtrType transFunc = ta1.GetTransitionFunction();
+
+		MTBDDType& mtbdd = transFunc->getMTBDD();
+
+		LeftHandSideType lhs;
+
+		StateType newInitialState = transFunc->AllocateState(0);
+		LeftHandSideType newLhs;
+		newLhs.push_back(newInitialState);
+
+		RootType root = mtbdd.Apply(transFunc->getRoot(0, lhs), transFunc->getRoot(1, lhs), leafUnion);
+
+		transFunc->setRoot(2, newLhs, root);
+
+		//TreeAutomaton result(
+		
+		mtbdd.DumpToDotFile("mtbdd.dot");
+
+
+		return ta1;
+	}
+};
+
+#endif
