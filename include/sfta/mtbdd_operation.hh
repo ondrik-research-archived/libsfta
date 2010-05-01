a9cf4cbec0c27953b4cc56bba3fbc6fcfb7cc8bd
Ondra Lengal <ondra@lengal.net>
2010-05-01 16:41:03 +0200

    MTBDDOperation: Intersection for N-ary nodes
     * intersection for n-nary nodes

--- include/sfta/mtbdd_operation.hh
+++ include/sfta/mtbdd_operation.hh
@@ -57,11 +57,15 @@ private:  // Private data types
 
 	typedef typename MTBDDType::LeafType LeafType;
 
+	typedef typename TransitionFunctionType::MTBDDRootTypeHashTable MTBDDRootTypeHashTable;
+
 	typedef std::pair<StateType, StateType> PairOfStatesType;
 	typedef std::pair<PairOfStatesType, StateType> PairOfStatesNewStateType;
 	typedef std::queue<PairOfStatesNewStateType> QueueOfPairsPlusNewStateType;
 	typedef std::set<StateType> SetOfStates;
 	typedef std::map<PairOfStatesType, StateType> MapOfPairsToStateType;
+
+	typedef typename SFTA::Private::Convert Convert;
 
 	class LeafIntersection
 		: public MTBDDType::AbstractApplyFunctorType
@@ -89,10 +93,10 @@ private:  // Private data types
 		StateType getNewStateForProduct(const PairOfStatesType& prod)
 		{
 			typename MapOfPairsToStateType::const_iterator it;
-			if ((it = translationTable_.find(prod)) == translationTable_.end())
+			if ((it = translationTable_->find(prod)) == translationTable_->end())
 			{	// in case the product state is not in the table
 				StateType newState = transFunc_->AllocateState(regToken_);
-				translationTable_.insert(std::make_pair(prod, newState));
+				translationTable_->insert(std::make_pair(prod, newState));
 				return newState;
 			}
 
@@ -104,7 +108,7 @@ private:  // Private data types
 
 		LeafIntersection(QueueOfPairsPlusNewStateType* workingQueue,
 			TransFuncPtrType transFunc, RegistrationTokenType regToken,
-			MapOfPairsToStateType translationTable)
+			MapOfPairsToStateType* translationTable)
 			: workingQueue_(workingQueue),
 				transFunc_(transFunc),
 				regToken_(regToken),
@@ -154,6 +158,17 @@ private:  // Private data types
 		}
 	};
 
+private:  // Private data members
+
+	/**
+	 * @brief  The name of the Log4cpp category for logging
+	 *
+	 * The name of the Log4cpp category used for logging messages from this
+	 * class.
+	 */
+	static const char* LOG_CATEGORY_NAME;
+
+
 public:   // Public methods
 
 	virtual TreeAutomaton Union(TreeAutomaton& ta1, TreeAutomaton& ta2) const
@@ -193,7 +208,7 @@ public:   // Public methods
 		MapOfPairsToStateType translationTable;
 		SetOfStates processedStates;
 		LeafIntersection intersectFunc(&queueOfProducts, transFunc,
-			result.GetRegToken(), translationTable);
+			result.GetRegToken(), &translationTable);
 		RootType root = mtbdd.Apply(transFunc->getRoot(ta1.GetRegToken(), lhs),
 			transFunc->getRoot(ta2.GetRegToken(), lhs), &intersectFunc);
 		transFunc->setRoot(result.GetRegToken(), lhs, root);
@@ -220,38 +235,173 @@ public:   // Public methods
 
 				if ((root1 != transFunc->sinkState_) && (root2 != transFunc->sinkState_))
 				{	// in case both states have successors
-					LeftHandSideType newLhs;
-					newLhs.push_back(newState);
+					LeftHandSideType newLhs(1);
+					newLhs[0] = newState;
 					root = mtbdd.Apply(root1, root2, &intersectFunc);
 					transFunc->setRootForArity1(result.GetRegToken(), newLhs, root);
 				}
 
 				// find roots for binary symbols with the states
 				lhs1 = LeftHandSideType(2);
+				lhs2 = LeftHandSideType(2);
 
 				// TODO: this could be done more efficiently using proper data
 				// structures!
 
 				// bind to the first position
+				lhs1[0] = prod.first;
+				lhs2[0] = prod.second;
 				for (size_t i = 0; i < transFunc->container2_.size(); ++i)
 				{
-					for (size_t j = 0; j < transFunc->container2_.size(); ++j)
-					{	// for each pair of states
-						if (translationTable.find(std::make_pair(i,j)))
-						{
-						}
+					lhs1[1] = i;
+					root1 = transFunc->getRootForArity2(ta1.GetRegToken(), lhs1);
 
-						transFunc->container2_[prod.first][i];
-						transFunc->container2_[prod.second][j];
+					if (root1 != transFunc->sinkState_)
+					{	// in case the first root is interesting
+						for (size_t j = 0; j < transFunc->container2_.size(); ++j)
+						{	// find another matching pair
+							lhs2[1] = j;
+							root2 = transFunc->getRootForArity2(ta2.GetRegToken(), lhs2);
+
+							if (root2 != transFunc->sinkState_)
+							{	// in case the second root is interesting
+								typename MapOfPairsToStateType::const_iterator it;
+								if ((it = translationTable.find(std::make_pair(i,j)))
+									!= translationTable.end())
+								{	// in case such a pair exists
+									transFunc->container2_[prod.first][i];
+									transFunc->container2_[prod.second][j];
+
+									LeftHandSideType newLhs(2);
+									newLhs[0] = newState;
+									newLhs[1] = it->second;
+									root = mtbdd.Apply(root1, root2, &intersectFunc);
+									transFunc->setRootForArity2(result.GetRegToken(), newLhs, root);
+								}
+							}
+						}
 					}
 				}
 
 				// bind to the second position
+				// TODO: check whether the position was looked at in the previous go
+				// (e.g. for (q1, q1))
+				lhs1[1] = prod.first;
+				lhs2[1] = prod.second;
+				for (size_t i = 0; i < transFunc->container2_.size(); ++i)
+				{
+					lhs1[0] = i;
+					root1 = transFunc->getRootForArity2(ta1.GetRegToken(), lhs1);
+
+					if (root1 != transFunc->sinkState_)
+					{	// in case the first root is interesting
+						for (size_t j = 0; j < transFunc->container2_.size(); ++j)
+						{	// find another matching pair
+							lhs2[0] = j;
+							root2 = transFunc->getRootForArity2(ta2.GetRegToken(), lhs2);
+
+							if (root2 != transFunc->sinkState_)
+							{	// in case the second root is interesting
+								typename MapOfPairsToStateType::const_iterator it;
+								if ((it = translationTable.find(std::make_pair(i,j)))
+									!= translationTable.end())
+								{	// in case such a pair exists
+									transFunc->container2_[i][prod.first];
+									transFunc->container2_[j][prod.second];
+
+									LeftHandSideType newLhs(2);
+									newLhs[1] = newState;
+									newLhs[0] = it->second;
+									root = mtbdd.Apply(root1, root2, &intersectFunc);
+									transFunc->setRootForArity2(result.GetRegToken(), newLhs, root);
+								}
+							}
+						}
+					}
+				}
+
+				// bind roots with n-ary symbols with the states
+				for (typename MTBDDRootTypeHashTable::const_iterator it
+					= transFunc->containerN_.begin();
+					it != transFunc->containerN_.end(); ++it)
+				{	// for all entries in the hash table
+					const LeftHandSideType& firstVector = it->first;
+					root1 = it->second;
+
+					SFTA_LOGGER_DEBUG("Possible LHS1: " + Convert::ToString(firstVector));
+
+					size_t position;
+					for (position = 0; position < firstVector.size(); ++position)
+					{ // check that the entry has x from (x, y)
+						if (firstVector[position] == prod.first)
+						{	// if we found x in the left-hand side
+							break;
+						}
+					}
+
+					if (position == firstVector.size())
+					{	// in case no state in the left-hand side was x
+						continue;
+					}
+
+					// find an entry with y from (x, y) on the same position as x
+					for (typename MTBDDRootTypeHashTable::const_iterator jt
+						= transFunc->containerN_.begin();
+						jt != transFunc->containerN_.end(); ++jt)
+					{	// for all entries in the hash table
+						const LeftHandSideType& secondVector = jt->first;
+						root2 = jt->second;
+
+						SFTA_LOGGER_DEBUG("Possible LHS2: " + Convert::ToString(secondVector));
+
+						if ((firstVector.size() != secondVector.size())
+							|| (secondVector[position] != prod.second))
+						{	// in case the required state is not at searched position (or if
+							// the size is different)
+							continue;
+						}
+
+						SFTA_LOGGER_DEBUG("size and position match");
+
+						size_t i;
+						LeftHandSideType newLhs(firstVector.size());
+						newLhs[position] = newState;
+						for (i = 0 ; i < firstVector.size(); ++i)
+						{	// try to bind remaining states
+							if (i != position)
+							{	// in case we try to bind position that is not already bound
+								typename MapOfPairsToStateType::const_iterator kt;
+								if ((kt = translationTable.find(std::make_pair(firstVector[i],
+									secondVector[i]))) == translationTable.end())
+								{	// in case there does not exist a product state
+									break;
+								}
+
+								newLhs[i] = kt->second;
+							}
+						}
+
+						if (i == firstVector.size())
+						{	// in case there exists product state for each respective
+							// component of the left-hand side of a rule
+							root = mtbdd.Apply(root1, root2, &intersectFunc);
+							transFunc->setRootForArityN(result.GetRegToken(), newLhs, root);
+						}
+					}
+				}
 			}
 		} while (!queueOfProducts.empty());
 
 		return result;
 	}
 };
+
+
+// Setting the logging category name for Log4cpp
+template
+<
+	typename TA
+>
+const char* SFTA::MTBDDOperation<TA>::LOG_CATEGORY_NAME = "mtbdd_operation";
 
 #endif
