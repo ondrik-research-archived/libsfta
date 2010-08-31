5cf473d437aa9df04b1825385285da09989ccc08
Ondra Lengal <ondra@lengal.net>
2010-08-31 17:54:05 +0200

    CUDDSharedMTBDD char->char test: add test of Apply function

--- tests/cudd_shared_mtbdd_cc_test.cc
+++ tests/cudd_shared_mtbdd_cc_test.cc
@@ -611,10 +611,59 @@ BOOST_AUTO_TEST_CASE(monadic_apply)
 	delete bdd;
 }
 
-//BOOST_AUTO_TEST_CASE(apply)
-//{
-//}
-//
+
+BOOST_AUTO_TEST_CASE(apply)
+{
+	ASMTBDDCC* bdd = new CuddMTBDDCC();
+
+	// load test cases
+	ListOfTestCasesType testCases;
+	ListOfTestCasesType failedCases;
+	loadStandardTests(testCases, failedCases);
+
+	RootType root = createMTBDDForTestCases(bdd, testCases);
+
+	// apply functor that squares values in leaves
+	class TimesApplyFunctor
+		: public ASMTBDDCC::AbstractApplyFunctorType
+	{
+	public:
+
+		virtual LeafType operator()(const LeafType& lhs, const LeafType& rhs)
+		{
+			return lhs * rhs;
+		}
+	};
+
+	TimesApplyFunctor func;
+
+	RootType timesRoot = bdd->Apply(root, root, &func);
+
+	for (ListOfTestCasesType::const_iterator itTests = testCases.begin();
+		itTests != testCases.end(); ++itTests)
+	{	// test that the test cases have been stored properly
+#if DEBUG
+		BOOST_TEST_MESSAGE("Finding stored " + *itTests);
+#endif
+		FormulaParser::ParserResultUnsignedType prsRes =
+			FormulaParser::ParseExpressionUnsigned(*itTests);
+		LeafType leafValue = static_cast<LeafType>(prsRes.first);
+		leafValue *= leafValue;
+		MyVariableAssignment asgn = varListToAsgn(prsRes.second);
+
+		ASMTBDDCC::LeafContainer res;
+		res.push_back(&leafValue);
+
+		BOOST_CHECK_MESSAGE(
+			compareTwoLeafContainers(bdd->GetValue(timesRoot, asgn), res),
+			*itTests + " != " + leafContainerToString(bdd->GetValue(timesRoot, asgn)));
+	}
+
+
+	delete bdd;
+}
+
+
 //BOOST_AUTO_TEST_CASE(serialization)
 //{
 //}
