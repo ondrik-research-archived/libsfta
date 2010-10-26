03777fa96806f7ed59a60257eaa15e8582ed6fda
Ondra Lengal <ondra@lengal.net>
2010-10-26 15:41:31 +0200

    NDSymbolicBUTreeAutomaton test: add better automata for union testing

--- tests/nd_symbolic_bu_tree_automaton_test.cc
+++ tests/nd_symbolic_bu_tree_automaton_test.cc
@@ -42,12 +42,10 @@ class NDSymbolicBUTreeAutomatonFixture : public LogFixture
 {
 public:   // Private data types
 
-	typedef SFTA::Private::CompactVariableAssignment<32> MySymbol;
-
 	typedef SFTA::AbstractAutomaton
 		<
 			unsigned,
-			MySymbol
+			SFTA::Private::CompactVariableAssignment<4>
 		> AA;
 
 	typedef SFTA::AbstractBUTreeAutomaton
@@ -61,7 +59,7 @@ public:   // Private data types
 		<
 			unsigned,
 			SFTA::OrderedVector<unsigned>,
-			MySymbol
+			AA::SymbolType
 		> AbstractMTBDDType;
 
 	typedef SFTA::CUDDSharedMTBDD
@@ -98,6 +96,112 @@ public:   // Private data types
 	typedef AA::Operation AAO;
 
 
+	typedef NDSBUTA::StateType State;
+	typedef NDSBUTA::SymbolType Symbol;
+	typedef NDSBUTA::LeftHandSideType LeftHandSide;
+	typedef NDSBUTA::InputRightHandSideType RightHandSide;
+
+protected:
+
+		Symbol SYMBOL_A;
+		Symbol SYMBOL_B;
+
+public:   // Public methods
+
+		NDSymbolicBUTreeAutomatonFixture()
+			: SYMBOL_A("0000"),
+				SYMBOL_B("0001")
+		{ }
+
+
+
+	/**
+	 *
+	 *          ___                  ___
+	 *      a  /   \       _    a   /   \
+	 *    ---->|q_1|----->|_|------>|q_2|
+	 *         \___/       A        \___/F
+	 *                     |          |
+	 *                     |----------|
+	 *
+	 */
+	NDSBUTA* constructAutomaton1(TTW* ttWrapper) const
+	{
+		NDSBUTA* result = new NDSBUTA(ttWrapper);
+
+		// insert a() -> {q_1}
+		LeftHandSide lhs_empty;
+		State state_q1 = result->AddState();
+		RightHandSide trans_empty_a;
+		trans_empty_a.insert(state_q1);
+		result->AddTransition(lhs_empty, SYMBOL_A, trans_empty_a);
+
+		// insert a(q_1) -> {q_2}
+		LeftHandSide lhs_q1;
+		lhs_q1.push_back(state_q1);
+		State state_q2 = result->AddState();
+		RightHandSide trans_q1_a;
+		trans_q1_a.insert(state_q2);
+		result->AddTransition(lhs_q1, SYMBOL_A, trans_q1_a);
+
+		// insert a(q_2) -> {q_2}
+		LeftHandSide lhs_q2;
+		lhs_q2.push_back(state_q2);
+		RightHandSide trans_q2_a = trans_q1_a;
+		result->AddTransition(lhs_q2, SYMBOL_A, trans_q2_a);
+
+		return result;
+	}
+
+	/**
+	 *
+	 *          ___                  ___
+	 *      a  /   \       _    a   /   \
+	 *    ---->|q_1|----->|_|------>|q_2|
+	 *         \___/       |        \___/F
+	 *                     |    ___
+	 *                    b|   /   \      _
+	 *                     |-->|q_3|---->|_|
+	 *                         \___/F     |
+	 *                           A        |
+	 *                           |    b   |
+	 *                           |--------|
+	 *
+	 *
+	 */
+	NDSBUTA* constructAutomaton2(TTW* ttWrapper) const
+	{
+		NDSBUTA* result = new NDSBUTA(ttWrapper);
+
+		// insert a() -> {q_1}
+		LeftHandSide lhs_empty;
+		State state_q1 = result->AddState();
+		RightHandSide trans_empty_a;
+		trans_empty_a.insert(state_q1);
+		result->AddTransition(lhs_empty, SYMBOL_A, trans_empty_a);
+
+		// insert a(q_1) -> {q_2}
+		LeftHandSide lhs_q1;
+		lhs_q1.push_back(state_q1);
+		State state_q2 = result->AddState();
+		RightHandSide trans_q1_a;
+		trans_q1_a.insert(state_q2);
+		result->AddTransition(lhs_q1, SYMBOL_A, trans_q1_a);
+
+		// insert b(q_1) -> {q_3}
+		State state_q3 = result->AddState();
+		RightHandSide trans_q1_b;
+		trans_q1_b.insert(state_q3);
+		result->AddTransition(lhs_q1, SYMBOL_B, trans_q1_b);
+
+		// insert b(q_3) -> {q_3}
+		LeftHandSide lhs_q3;
+		lhs_q3.push_back(state_q3);
+		RightHandSide trans_q3_b = trans_q1_b;
+		result->AddTransition(lhs_q3, SYMBOL_B, trans_q3_b);
+
+		return result;
+	}
 };
 
 
@@ -108,18 +212,26 @@ public:   // Private data types
 
 BOOST_FIXTURE_TEST_SUITE(suite, NDSymbolicBUTreeAutomatonFixture)
 
-BOOST_AUTO_TEST_CASE(adding_transitions)
+BOOST_AUTO_TEST_CASE(operation_union)
 {
-	AA* ta = new NDSBUTA();
-	AAO* oper = ta->GetOperation();
-	AA* unionTa = oper->Union(ta, ta);
+	NDSBUTA* taInvalid = new NDSBUTA();
 
-	BOOST_TEST_MESSAGE("Input automaton:\n" + ta->ToString());
+	NDSBUTA* ta1 = constructAutomaton1(taInvalid->GetTTWrapper());
+
+	NDSBUTA* ta2 = constructAutomaton2(taInvalid->GetTTWrapper());
+
+	BOOST_TEST_MESSAGE("A1:\n" + ta1->ToString());
+	BOOST_TEST_MESSAGE("A2:\n" + ta2->ToString());
+
+	AAO* oper = ta1->GetOperation();
+	AA* unionTa = oper->Union(ta1, ta2);
+
 	BOOST_TEST_MESSAGE("Union automaton:\n" + unionTa->ToString());
 
 	delete unionTa;
 	delete oper;
-	delete ta;
+	delete ta1;
+	delete ta2;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
