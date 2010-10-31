4d849e3189db309a0a469dfef85474fd8ce2e0fc
Ondra Lengal <ondra@lengal.net>
2010-10-31 11:11:22 +0100

    AbstractAutomaton: add SetStateFinal() and IsStateFinal() methods

--- include/sfta/abstract_automaton.hh
+++ include/sfta/abstract_automaton.hh
@@ -175,6 +175,10 @@ public:   // Public methods
 
 	virtual StateType AddState() = 0;
 
+	virtual void SetStateFinal(const StateType& state) = 0;
+
+	virtual bool IsStateFinal(const StateType& state) const = 0;
+
 
 	virtual std::string ToString() const = 0;
 
