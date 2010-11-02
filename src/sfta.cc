171a31968300b830193390102a5e32dac7d460b2
Ondra Lengal <ondra@lengal.net>
2010-11-02 18:11:47 +0100

    SFTA: add stub for the user interface program

---
+++ src/sfta.cc
@@ -0,0 +1,19 @@
+/*****************************************************************************
+ *  Symbolic Finite Tree Automata Library
+ *
+ *  Copyright (c) 2010  Ondra Lengal <ondra@lengal.net>
+ *
+ *  Description:
+ *    Source file for sfta program, which is an interface to libSFTA.
+ *
+ *****************************************************************************/
+
+
+
+
+int main(int argc, char* argv[])
+{
+	assert(false);
+
+	return EXIT_SUCCESS;
+}
