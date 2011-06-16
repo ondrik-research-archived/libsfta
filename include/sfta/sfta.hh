c6e8728aaf217363536265e2f9df833dba901e80
Ondra Lengal <ondra.lengal@gmail.com>
2011-06-16 16:22:51 +0200

    sfta/sfta.hh: add macros GCC_DIAG_OFF and GCC_DIAG_ON
    Add two macros that enable selectively turning off and on certain GCC
    warnings. These macros work from GCC 4.6.

--- include/sfta/sfta.hh
+++ include/sfta/sfta.hh
@@ -29,3 +29,21 @@
 #define SFTA_LOGGER_CRIT(msg)   (SFTA_LOGGER_LOG_MESSAGE(crit, msg))
 #define SFTA_LOGGER_ALERT(msg)  (SFTA_LOGGER_LOG_MESSAGE(alert, msg))
 #define SFTA_LOGGER_FATAL(msg)  (SFTA_LOGGER_LOG_MESSAGE(fatal, msg))
+
+#if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 402
+#define GCC_DIAG_STR(s) #s
+#define GCC_DIAG_JOINSTR(x,y) GCC_DIAG_STR(x ## y)
+# define GCC_DIAG_DO_PRAGMA(x) _Pragma (#x)
+# define GCC_DIAG_PRAGMA(x) GCC_DIAG_DO_PRAGMA(GCC diagnostic x)
+# if ((__GNUC__ * 100) + __GNUC_MINOR__) >= 406
+#  define GCC_DIAG_OFF(x) GCC_DIAG_PRAGMA(push) \
+	         GCC_DIAG_PRAGMA(ignored GCC_DIAG_JOINSTR(-W,x))
+#  define GCC_DIAG_ON(x) GCC_DIAG_PRAGMA(pop)
+# else
+#  define GCC_DIAG_OFF(x) GCC_DIAG_PRAGMA(ignored GCC_DIAG_JOINSTR(-W,x))
+#  define GCC_DIAG_ON(x)  GCC_DIAG_PRAGMA(warning GCC_DIAG_JOINSTR(-W,x))
+# endif
+#else
+# define GCC_DIAG_OFF(x)
+# define GCC_DIAG_ON(x)
+#endif
